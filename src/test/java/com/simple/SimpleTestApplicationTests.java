a0d030366569439bf6749243914e2a3c139db676
Leandro Corbelo <leandro.corbelo@matera.com>
2024-03-06 10:25:06 -0300

    Add project files

---
+++ src/test/java/com/simple/SimpleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matera.cnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
